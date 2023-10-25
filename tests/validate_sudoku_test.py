306a6977a5e4dabee4a0d99c533a63e8de021d51
Leanne Keeley <sonetteira@users.noreply.github.com>
2023-10-25 17:38:58 -0400

    Update validate_sudoku_test.py

--- tests/validate_sudoku_test.py
+++ tests/validate_sudoku_test.py
@@ -6,7 +6,7 @@ def test_import():
     assert 'numpy' in importlib.sys.modules
 
 def test_validate_sudoku(capsys):
-    output = r"[^\n]+[^in]valid[^\n]*\n[^\n]+invalid[^\n]*\n[^\n]+[^in]valid[^\n]*\n[^\n]+invalid[^\n]*"
+    output = r"^[^in]*valid[^\n]*\n[^\n]*invalid[^\n]*\n[^in]*valid[^\n]*\n[^\n]*invalid[^\n]*"
     validate_sudoku.main()
     captured = capsys.readouterr()
     assert re.search(output, captured.out, re.IGNORECASE)
